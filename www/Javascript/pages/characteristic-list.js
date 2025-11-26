418c1a7f7e79a911abe2be341a63f6e1764c860b
fabiojunik <fabiojunik@gmail.com>
2025-11-26 16:11:24 +0000

    style: improve characteristic sytles

--- www/Javascript/pages/characteristic-list.js
+++ www/Javascript/pages/characteristic-list.js
@@ -15,16 +15,16 @@ export function CharacteristicPage() {
         const li = document.createElement("li");
         li.classList.add("genus-item");
 
-        const a = document.createElement("a");
-        a.textContent = item.description;
-        a.classList.add("genus-link");
-        a.classList.add(Orchid.getCharacteristicClass(currentyCharacteristic, item.id));
+        const descriptionEl = document.createElement("p");
+        descriptionEl.textContent = item.description;
+        li.classList.add("genus-link");
+        li.classList.add(Orchid.getCharacteristicClass(currentyCharacteristic, item.id));
 
         if (currentyCharacteristic === "genus") {
-          a.setAttribute("id", item.description.toLocaleLowerCase());
+          li.setAttribute("id", item.description.toLocaleLowerCase());
         }
 
-        li.appendChild(a);
+        li.appendChild(descriptionEl);
         li.addEventListener("click", () => {
           const search = `?characteristic=${currentyCharacteristic}`;
           navigateTo(`${search}&characteristic-id=${item.id}`);
